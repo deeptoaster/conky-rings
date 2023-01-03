4963d6a1ccb3d30586ed9b9a517906ce10ae6b47
Deep Toaster <deeptoaster@gmail.com>
2023-01-03 17:12:50 -0500

    add CPU and memory breakdowns and clean up ring drawing

--- rings.lua
+++ rings.lua
@@ -6,226 +6,161 @@ config = {
   network_ethernet = 'eno1',
   network_wlan = 'wlo1'
 }
-elements = {
+groups = {
   {
-    x0 = 0,
-    y0 = 79,
-    x1 = 337,
-    y1 = 79,
-    width = 2
+    {
+      breakpoints = { 'top cpu 1', 'top cpu 2', 'top cpu 3', 'top cpu 4' },
+      command = 'cpu',
+      max = 100
+    },
+    { command = 'acpitemp', max = 100 }
   },
   {
-    name = 'cpu',
-    arg = '',
-    max = 100,
-    x = 400,
-    y = 80,
-    r = 60,
-    width = 6,
-    start_angle = -90,
-    end_angle = 180
+    {
+      breakpoints = {
+        'top_mem mem 1',
+        'top_mem mem 2',
+        'top_mem mem 3',
+        'top_mem mem 4'
+      },
+      command = 'memperc',
+      max = 100
+    },
+    { command = 'swapperc', max = 100 }
   },
   {
-    name = 'acpitemp',
-    arg = '',
-    max = 100,
-    x = 400,
-    y = 80,
-    r = 52,
-    width = 6,
-    start_angle = -90,
-    end_angle = 180
+    {
+      command =
+          "execi 1 df --output=pcent -x tmpfs | sed -En '2s/\\s*(\\S+)%/\\1/p'",
+      max = 100
+    },
+    {
+      command =
+          "execi 1 df --output=pcent -x tmpfs | sed -En '3s/\\s*(\\S+)%/\\1/p'",
+      max = 100
+    },
+    {
+      command =
+          "execi 1 df --output=pcent -x tmpfs | sed -En '4s/\\s*(\\S+)%/\\1/p'",
+      max = 100
+    },
+    {
+      command =
+          "execi 1 df --output=pcent -x tmpfs | sed -En '5s/\\s*(\\S+)%/\\1/p'",
+      max = 100
+    },
+    {
+      command =
+          "execi 1 df --output=pcent -x tmpfs | sed -En '6s/\\s*(\\S+)%/\\1/p'",
+      max = 100
+    }
   },
   {
-    x0 = 0,
-    y0 = 239,
-    x1 = 337,
-    y1 = 239,
-    width = 2
-  },
-  {
-    name = 'memperc',
-    arg = '',
-    max = 100,
-    x = 400,
-    y = 240,
-    r = 60,
-    width = 6,
-    start_angle = -90,
-    end_angle = 180
-  },
-  {
-    name = 'swapperc',
-    arg = '',
-    max = 100,
-    x = 400,
-    y = 240,
-    r = 52,
-    width = 6,
-    start_angle = -90,
-    end_angle = 180
-  },
-  {
-    x0 = 0,
-    y0 = 399,
-    x1 = 337,
-    y1 = 399,
-    width = 2
-  },
-  {
-    name = 'execi',
-    arg = "1 df --output=pcent -x tmpfs | sed -En '2s/\\s*(\\S+)%/\\1/p'",
-    max = 100,
-    x = 400,
-    y = 400,
-    r = 60,
-    width = 6,
-    start_angle = -90,
-    end_angle = 180
-  },
-  {
-    name = 'execi',
-    arg = "1 df --output=pcent -x tmpfs | sed -En '3s/\\s*(\\S+)%/\\1/p'",
-    max = 100,
-    x = 400,
-    y = 400,
-    r = 52,
-    width = 6,
-    start_angle = -90,
-    end_angle = 180
-  },
-  {
-    name = 'execi',
-    arg = "1 df --output=pcent -x tmpfs | sed -En '4s/\\s*(\\S+)%/\\1/p'",
-    max = 100,
-    x = 400,
-    y = 400,
-    r = 44,
-    width = 6,
-    start_angle = -90,
-    end_angle = 180
-  },
-  {
-    name = 'execi',
-    arg = "1 df --output=pcent -x tmpfs | sed -En '5s/\\s*(\\S+)%/\\1/p'",
-    max = 100,
-    x = 400,
-    y = 400,
-    r = 36,
-    width = 6,
-    start_angle = -90,
-    end_angle = 180
-  },
-  {
-    x0 = 0,
-    y0 = 559,
-    x1 = 337,
-    y1 = 559,
-    width = 2
-  },
-  {
-    name = 'downspeedf',
-    arg = config['network_ethernet'],
-    max = 12,
-    log = true,
-    x = 400,
-    y = 560,
-    r = 60,
-    width = 6,
-    start_angle = -90,
-    end_angle = 180
-  },
-  {
-    name = 'downspeedf',
-    arg = config['network_wlan'],
-    max = 12,
-    log = true,
-    x = 400,
-    y = 560,
-    r = 52,
-    width = 6,
-    start_angle = -90,
-    end_angle = 180
-  },
-  {
-    name = 'upspeedf',
-    arg = config['network_ethernet'],
-    max = 12,
-    log = true,
-    x = 400,
-    y = 560,
-    r = 44,
-    width = 6,
-    start_angle = -90,
-    end_angle = 180
-  },
-  {
-    name = 'upspeedf',
-    arg = config['network_wlan'],
-    max = 12,
-    log = true,
-    x = 400,
-    y = 560,
-    r = 36,
-    width = 6,
-    start_angle = -90,
-    end_angle = 180
+    {
+      command = 'downspeedf ' .. config['network_ethernet'],
+      log = true,
+      max = 12
+    },
+    {
+      command = 'downspeedf ' .. config['network_wlan'],
+      log = true,
+      max = 12
+    },
+    {
+      command = 'upspeedf ' .. config['network_ethernet'],
+      log = true,
+      max = 12
+    },
+    { command = 'upspeedf ' .. config['network_wlan'], log = true, max = 12 }
   }
 }
 require 'cairo'
-function rgba(color, alpha)
-  return color / 0x10000 % 0x100 / 255., color / 0x100 % 0x100 / 255.,
-      color % 0x100 / 255., alpha
+function angle(position, max)
+  return position / max * math.pi * 1.5 - math.pi
 end
-function draw_line(cr, pt)
-  cairo_move_to(cr, pt['x0'], pt['y0'])
-  cairo_line_to(cr, pt['x1'], pt['y1'])
-  cairo_set_source_rgba(cr, rgba(config['fg_color'], config['fg_alpha']))
-  cairo_set_line_width(cr, pt['width'])
-  cairo_stroke(cr)
-end
-function draw_ring(cr, val, pt)
-  local angle_0 = pt['start_angle'] * math.pi / 180 - math.pi / 2
-  local angle_f = pt['end_angle'] * math.pi / 180 - math.pi / 2
-  local angle_t = angle_0 + val / pt['max'] * (angle_f - angle_0)
-  cairo_arc(cr, pt['x'], pt['y'], pt['r'], angle_0, angle_f)
-  cairo_set_source_rgba(cr, rgba(config['bg_color'], config['bg_alpha']))
-  cairo_set_line_width(cr, pt['width'])
-  cairo_stroke(cr)
-  cairo_arc(cr, pt['x'], pt['y'], pt['r'], angle_0, angle_t)
-  cairo_set_source_rgba(cr, rgba(config['fg_color'], config['fg_alpha']))
-  cairo_stroke(cr)
-end
-function setup_rings(cr, pt)
-  if pt['name'] == nil then
-    draw_line(cr, pt)
+function evaluate(command, log)
+  local value = conky_parse(string.format('${%s}', command)):gsub('%%', '')
+  value = tonumber(value)
+  if value == nil then
+    return
+  elseif log then
+    return math.log(value + 1)
   else
-    local val = conky_parse(
-      string.format('${%s %s}', pt['name'], pt['arg'])
-    ):gsub('%%', '')
-    val = tonumber(val)
-    if val == nil then
-      return
-    end
-    if pt['log'] then
-      val = math.log(val + 1)
-    end
-    draw_ring(cr, val, pt)
+    return value
   end
+end
+function rgba(color, alpha)
+  return color / 0x10000 % 0x100 / 255, color / 0x100 % 0x100 / 255,
+      color % 0x100 / 255, alpha
+end
+function draw_line(cairo, y, breakpoint_count)
+  cairo_set_line_width(cairo, 2)
+  cairo_set_source_rgba(cairo, rgba(config['fg_color'], config['fg_alpha']))
+  cairo_move_to(cairo, 0, y - 1)
+  cairo_line_to(cairo, 337 - breakpoint_count * 2, y - 1)
+  cairo_stroke(cairo)
+end
+function draw_ring(cairo, y, radius, breakpoints, max)
+  local previous_angle = angle(0, max)
+  cairo_set_source_rgba(cairo, rgba(config['fg_color'], config['fg_alpha']))
+  for breakpoint_index in pairs(breakpoints) do
+    local breakpoint_angle = angle(breakpoints[breakpoint_index], max)
+    if breakpoint_angle > previous_angle then
+      cairo_set_line_width(cairo, (#breakpoints - breakpoint_index) * 2 + 6)
+      cairo_arc(
+        cairo,
+        400,
+        y,
+        radius + #breakpoints - breakpoint_index,
+        previous_angle,
+        breakpoint_angle
+      )
+      cairo_stroke(cairo)
+      previous_angle = breakpoint_angle + math.pi / 135
+    end
+  end
+  cairo_set_line_width(cairo, 6)
+  cairo_set_source_rgba(cairo, rgba(config['bg_color'], config['bg_alpha']))
+  cairo_arc(cairo, 400, y, radius, previous_angle, angle(max, max))
+  cairo_stroke(cairo)
 end
 function conky_rings()
   if conky_window == nil then
     return
   end
-  local cs = cairo_xlib_surface_create(
+  local cairo = cairo_create(cairo_xlib_surface_create(
     conky_window.display,
     conky_window.drawable,
     conky_window.visual,
     conky_window.width,
     conky_window.height
-  )
-  local cr = cairo_create(cs)
-  for i in pairs(elements) do
-    setup_rings(cr, elements[i])
+  ))
+  for group_index in pairs(groups) do
+    local y = group_index * 160 - 80
+    local breakpoint_count = 0
+    for ring_index in pairs(groups[group_index]) do
+      local breakpoints = {}
+      local ring = groups[group_index][ring_index]
+      local value = evaluate(ring['command'], ring['log'])
+      if ring['breakpoints'] ~= nil then
+        local position = 0
+        for breakpoint_index in pairs(ring['breakpoints']) do
+          local value = evaluate(ring['breakpoints'][breakpoint_index], ring['log'])
+          if value == nil then
+            break
+          end
+          position = position + value
+          table.insert(breakpoints, position)
+        end
+      end
+      if value ~= nil then
+        table.insert(breakpoints, value)
+      end
+      breakpoint_count = math.max(breakpoint_count, #breakpoints)
+      if #breakpoints ~= 0 then
+        draw_ring(cairo, y, 68 - ring_index * 8, breakpoints, ring['max'])
+      end
+    end
+    draw_line(cairo, y, breakpoint_count)
   end
 end
